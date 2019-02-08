af2ccc85fac5420c0543badcebc9d8471c3d012f
Günther von Häfen <>
2019-02-08 05:55:32 +0100

    1.8 Edited all code and functions into JQuery. LoadList() needs to be finished.

---
+++ js/scripts.js
@@ -0,0 +1,152 @@
+var starwarsRepository = (function () {
+// Array with Star Wars Characters as Objects
+  var characters = [];
+
+// API URL
+  var apiURL = 'https://akabab.github.io/starwars-api/api/all.json';
+
+// function to add characters
+  function add(item) {
+    characters.push(item);
+  }
+
+// function to select characters
+  function getAll() {
+    return characters;
+  }
+
+// function to add List item to DOM
+  function addListItem(character) {
+    var $unorderedList = $('ul');
+    // create necessary elements
+    var $newListElement = $('<li></li>');
+    var $newSpanElement = $('<span>' + character.name + '</span>');
+    var $newButtonElement = $('<button>+</button>');
+    // add the classes
+    $newListElement.addClass('content__item');
+    $newButtonElement.addClass('content__item--button');
+    //append new elements
+    $unorderedList.append($newListElement);
+    $newListElement.append($newSpanElement);
+    $newListElement.append($newButtonElement);
+    // add event listener
+    $newButtonElement.on('click', function (event) {
+      showDetails(character);
+    });
+    $newButtonElement.on('click', function (event) {
+      var audio = new Audio('files/lasrhit4.mp3');
+      audio.play();
+    });
+  }
+
+// modal functions (show/hide)
+  function showModal(title, mass, height, gender, homeworld, species, more, image) {
+    var $modalContainer = $('#modal-container');
+    $modalContainer.empty(); // clear all content
+
+    var modal = $('<div class="modal"></div>'); // create inner modal div
+
+    // adding the new content
+    var closeButtonElement = $('<div class="button-element"></div>'); // close button
+    var closeButton = $('<button class="modal-close">Close</button>');
+    closeButton.on('click', hideModal);
+
+    var imageContainer = $('<div class="image-container"></div>'); // image
+    var characterImage = $('<img src="' + image + '" class="character-image">');
+
+    var titleElement = $('<h1>' + title + '<h1>'); // create title element
+
+    var contentElementMass = $('<p>Mass: ' + mass + '</p>'); // create content elements
+    var contentElementHeight = $('<p>Height: ' + height + '</p>');
+    var contentElementGender = $('<p>Gender: ' + gender + '</p>');
+    var contentElementHomeworld = $('<p>Homeworld: ' + homeworld + '</p>');
+    var contentElementSpecies = $('<p>Species: ' + species + '</p>');
+    var contentElementMore = $('<p>More Details: </p>');
+    var contentElementMoreLink = $('<a href="' + more + '" target="_blank">Here</a>');
+
+    // apoending elements
+    modal.append(closeButtonElement);
+    closeButtonElement.append(closeButton);
+    modal.append(imageContainer);
+    imageContainer.append(characterImage);
+    imageContainer.append(titleElement);
+    modal.append(contentElementMass);
+    modal.append(contentElementHeight);
+    modal.append(contentElementGender);
+    modal.append(contentElementHomeworld);
+    modal.append(contentElementSpecies);
+    modal.append(contentElementMore);
+    contentElementMore.append(contentElementMoreLink);
+    $modalContainer.append(modal);
+
+    $modalContainer.addClass('is-visible'); // add class to show modal
+  }
+
+  function hideModal() {
+    var $modalContainer = $('#modal-container');
+    $modalContainer.removeClass('is-visible'); // remove class to hide modal
+  }
+
+// function for showing the details of the characters in a modal
+  function showDetails(item) {
+    showModal(item.name, item.mass, item.height, item.gender, item.homeworld, item.species, item.more, item.img);
+  }
+
+// loading the characters from API
+  function loadList() {
+    $.ajax(apiURL, {dataType: 'json'})
+      .then(function (response) {
+        return response.responseJSON;
+      }).then(function (json) {
+        $.response.each(function (item) {
+          var character = {
+            name: $(this).name,
+            mass: $(this).mass,
+            height: $(this).height,
+            gender: $(this).gender,
+            homeworld: $(this).homeworld,
+            species: $(this).species,
+            more: $(this).wiki,
+            img: $(this).image
+          };
+          add(character);
+        });
+     }).catch(function (e) {
+          console.error(e);
+     });
+  }
+
+// eventListeners for closing the modal by pressing ESC or clicking outside modal
+  $('window').on('keydown', function(event) { // press ESC
+    var $modalContainer = $('#modal-container');
+
+    if (event.key === "Escape" && $modalContainer.hasClass('is-visible')) {
+      hideModal();
+    }
+  });
+
+  var $modalContainer = $('#modal-container'); // click outside modal
+  $modalContainer.on('click', function(event) {
+    var target = event.target;
+    if (target === $modalContainer) {
+      hideModal();
+    }
+  });
+
+  return {
+   add: add,
+   getAll: getAll,
+   addListItem: addListItem,
+   showDetails: showDetails,
+   loadList: loadList
+  };
+
+})();
+
+// Call loadList() to create the elements for the DOM
+
+starwarsRepository.loadList().then(function() {
+  starwarsRepository.getAll().forEach(function(character){
+    starwarsRepository.addListItem(character);
+  });
+});
